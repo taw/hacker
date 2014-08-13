0938a5f2c932a1feada54ce12ad45efdb88e177e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-08-13 12:49:05 +0100

    Optimization by regexp is a dead end

--- optimize_branches.rb
+++ optimize_branches.rb
@@ -39,13 +39,71 @@ def simple_funcall_propagation!
   end
 end
 
+def xorfun_propagation!
+  xorfuns = {}
+  @branches.gsub!(
+  %r[long\s+([a-zA-Z0-9]+)\(long\s+paramLong\)
+    \s*\{\s*
+    return\s+(0x[A-F0-9]+)\s+\^\s+paramLong;
+    \s*\}\s*
+  ]x){
+    xorfuns[$1] = $2
+    ""
+  }
+  xorfuns.each do |k,v|
+    @branches.gsub!(%r[\s#{k}(\( [^\(\)]+ \))]x) { " (#{v} ^ #{$1})" }
+  end
+end
+
+def zeroq_optimize!
+  zeroq = {}
+  @branches.gsub!(
+  %r[long\s+([a-zA-Z0-9]+)\(long\s+paramLong\)
+    \s*\{\s*
+    if\s+\(paramLong\s+==\s+0L\)\s+
+      return\s+(\d+L);\s+
+    return\s+(\d+L);\s+
+    \s*\}\s*
+  ]x){
+    zeroq[$1] = "== 0 ? #{$2} : #{$3}"
+    ""
+  }
+  zeroq.each do |k,v|
+    @branches.gsub!(%r[\s#{k}(\( [^\(\)]+ \))]x) { " (#{$1} #{v})" }
+  end
+end
+
+def minusone_optimize!
+  minusone_fun = {}
+  @branches.gsub!(
+  %r[long\s+([a-zA-Z0-9]+)\(long\s+paramLong\)
+    \s*\{\s*
+    return\s+([a-zA-Z0-9]+)\(paramLong\s+-\s+1L\);
+    \s*\}\s*
+  ]x){
+    minusone_fun[$1] = $2
+    ""
+  }
+  minusone_fun.each do |k,v|
+    @branches.gsub!(%r[\s#{k}(\( [^\(\)]+ \))]x) { " #{v}(#{$1} - 1L)" }
+  end
+end
+
 @branches = open("Branches.java").read()
 STDERR.puts @branches.size
 
 optimize_useless_return!
 5.times do
   constant_propagation!
+end
+5.times do
+  zeroq_optimize!
+  minusone_optimize!
+  constant_propagation!
   simple_funcall_propagation!
+  xorfun_propagation!
+  @branches.gsub!(" (paramLong)", " paramLong")
+  @branches.gsub!("(paramLong - 1L) - 1L", "paramLong - 2L")
 end
 
 STDERR.puts @branches.size
