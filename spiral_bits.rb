b80f214745bad790465a4226b3e75bb328f21245
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2014-08-26 02:05:48 +0100

    6 conflicts, maybe just fix them by hand?

--- spiral_bits.rb
+++ spiral_bits.rb
@@ -5,6 +5,25 @@ require "pp"
 require "set"
 require_relative "pnm"
 
+module Enumerable
+  def all_min_by
+    res = []
+    mindist = nil
+
+    each do |elem|
+      dist = yield(elem)
+      if mindist.nil? or dist < mindist
+        res = [elem]
+        mindist = dist
+      elsif dist == mindist
+        res << elem
+      end
+    end
+
+    [mindist, res]
+  end
+end
+
 def pixel_dist((a,b,c), (d,e,f))
   Math.sqrt((a-b)**2 + (b-e)**2 + (c-f)**2)
 end
@@ -35,7 +54,7 @@ def pixel_map_force(px)
 end
 
 def pixel_map_threshold(px, thr=20)
-  bg  = [0,0,0]
+  bg  = [0, 0, 0]
   px1 = [255, 255, 255]
   px0 = [35, 182, 153]
 
@@ -59,6 +78,11 @@ def read_spiral_pixels
     end
   end
   [x, y, data]
+end
+
+def cleanup!
+  x, y, pixels = read_spiral_pixels
+  PNM.save("cleaned.pnm", x, y, pixels.flatten.pack("C*"))
 end
 
 def group_into_sets
@@ -102,21 +126,62 @@ end
 
 def save_sets_pic(sets)
   out = "\x00" * (1024*1024*3)
-
   sets.each do |set|
     color = [rand(256), rand(256), rand(256)].pack("CCC")
     set.each do |x,y,c|
       out[3*x + 3*1024*y, 3] = color
     end
   end
-
   PNM.save("sets.pnm", 1024, 1024, out)
 end
 
-unless Pathname("cleaned.pnm").exist?
-  x, y, pixels = read_spiral_pixels
-  PNM.save("cleaned.pnm", x, y, pixels)
+def set_distance(a,b)
+  return 1000 if a[:xmin] >= b[:xmax] + 10
+  return 1000 if b[:xmin] >= a[:xmax] + 10
+  return 1000 if a[:ymin] >= b[:ymax] + 10
+  return 1000 if b[:ymin] >= a[:ymax] + 10
+  a[:elems].map{|x1,y1,_|
+    b[:elems].map{|x2,y2,_|  (x1-x2)**2 + (y1-y2)**2 }.min
+  }.min
 end
+
+def follow_the_path(sets)
+  sets = sets.map{|set|
+    xs = set.map{|x,_,_| x}
+    ys = set.map{|_,y,_| y}
+    bit = set.first[-1]
+    elems = set.map{|x,y,_| [x,y]}
+    {xmin: xs.min, xmax: xs.max, ymin: ys.min, ymax: ys.max, bit: bit, elems: elems, size: set.size}
+  }
+  result = []
+  sets = Set[*sets]
+  current = sets.max_by{|set| set[:size]}
+
+  while true
+    sets.delete current
+    result << current[:bit]
+
+    return if sets.empty?
+
+    mindist, neighbours = sets.all_min_by{|set|
+      set_distance(set, current)
+    }
+
+    if neighbours.size > 1
+      #require 'pry'; binding.pry
+      warn "Multiple neighbours: #{neighbours.size} #{current[:elemns]}"
+    end
+
+    # puts "Bit #{current[:bit]}, Distance #{mindist}, #{sets.size} left"
+    current = neighbours[0]
+  end
+
+  # require 'pry'; binding.pry
+end
+
+cleanup! # unless Pathname("cleaned.pnm").exist?
 
 sets = group_into_sets
 save_sets_pic(sets)
+follow_the_path(sets)
+
